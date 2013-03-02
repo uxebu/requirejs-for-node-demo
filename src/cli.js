bbe449453ed895467fa52c5a5f6ef54bf8bd95f1
Wolfram Kriesing <wk@uxebu.com>
2013-03-02 12:35:05 +0100

    Remove a parameter requirement.

--- src/cli.js
+++ src/cli.js
@@ -2,11 +2,9 @@
 // All code that is ONLY needed for the cli is contained
 // in here, this file is only used in the `/bin/<filename>.js`!
 
-//var requirejs = require('./requireSetup')(require);
 var stats = require('./main').stats;
 
 var argv = require('optimist')
-    .demand('_')
     .usage('Usage: $0 filename.swf')
     .argv;
 var swfFileName = argv._[0];
